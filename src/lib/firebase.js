99339021e965100dc5080336bc4f15dd44bee681
Your Name <you@example.com>
2022-09-22 09:04:18 -0400

    ingrasar con usuario creado

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -70,17 +70,17 @@ const unsub = (callback) => {
 
 //export const createUser = (email, password) =>
 export const ingresarConUsuario = (email, password) => {
-const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password) 
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+  const auth = getAuth();
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 }
 
 //const auth = getAuth();
